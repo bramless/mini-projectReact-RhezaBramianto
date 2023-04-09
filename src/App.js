f6059144d1d58c9e3d479ca7da65b7bc622a1637
bramless <bramless@gmail.com>
2023-04-09 11:15:47 +0700

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMovieList, searchMovie } from "./api"
+import { useEffect, useState } from "react"
 
-function App() {
+const App = () => {
+   const [popuplarMovies, setPopularMovies] = useState([])
+  
+  useEffect(() => {
+    getMovieList().then((result) =>{
+      setPopularMovies(result)
+    })
+  }, [])
+
+  const PopuplarMoviesList = () => {
+    return popuplarMovies.map((movie, i) => {
+      return (
+        <div className="movieWrapper" key={i}>
+        <img className="movieImage" 
+          src={`${process.env.REACT_APP_BASEIMAGE}/${movie.poster_path}`} 
+        />
+        <div className="movieTitle">{movie.title}</div>
+        <div className="movieID">Movie ID: {movie.id}</div>
+        <div className="moviePopularity">Popularity : {movie.popularity}</div>
+        <div className="movieOverview">{movie.overview}</div>
+      </div>
+      )
+
+    })
+  }
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    }
+  }
+
+  console.log({ popuplarMovies: popuplarMovies })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie Data Base</h1>
+        <input
+          placeholder="Seacrh Movie" className="movieSearch"
+          onChange={({ target }) => search(target.value)} />
+        <div className="movieContainer">
+          <PopuplarMoviesList />
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
